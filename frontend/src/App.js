5ec86e5335fb070fa53694bae33a2f1b58316844
amitgawande369 <amitgawande002@gmail.com>
2022-12-16 16:03:47 +0530

    1 commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>products list</main>
+          </div>
+    </>
+  );
+}
+
+export default App;
