f9061dc220a99a5c454c6563441b7852d68a676e
amitgawande369 <amitgawande002@gmail.com>
2022-12-17 15:17:35 +0530

    2 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import data from "./data";
 
 function App() {
   return (
@@ -7,7 +7,28 @@ function App() {
       <header>
         <a href="/">Amazon</a>
       </header>
-      <main>products list</main>
+      <main> 
+        <h1>featured products</h1>
+        <div className="products">
+          {data.products.map((product) => {
+            return <div className="product" key={product.slug}>
+              {/* {console.log(`/product/${product.slug}`)} */}
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          })}
+        </div>
+        </main>
           </div>
     </>
   );
