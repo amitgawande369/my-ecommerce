a239df046f31e8f27b8dc4d5619506a8109939af
amitgawande369 <amitgawande002@gmail.com>
2022-12-20 15:56:31 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
-import { express } from "express";
-import data from "./data";
+import  express  from "express";
+import data from './data.js'
 
 const app = express();
 
-app.get()
-//
+app.get('/api/products', (req, res) =>{
+    res.send(data.products)
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`);
+});
